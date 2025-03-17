b25fe7ac8ff46a52fe054d1c1195a3cee22d4e97
Amlali5 <125584098+Amlali5@users.noreply.github.com>
2025-03-17 23:54:59 +0200

    dockerKubePipeline.groovy

--- vars/dockerKubePipeline.groovy
+++ vars/dockerKubePipeline.groovy
@@ -4,13 +4,14 @@ def call(Map config) {
         stages {
             stage('Clone Repository') {
                 steps {
+                    echo 'Cloning repository...'
                     checkout([$class: 'GitSCM', branches: [[name: config.branch]],
                               userRemoteConfigs: [[url: config.repositoryUrl, credentialsId: config.gitCredentialsId]]])
                 }
             }
             stage('Verify Files') { 
                 steps {
-                    echo 'Checking if static_website directory exists...'
+                    echo 'Verifying if static_website directory exists...'
                     sh 'ls -la jenkins/lab3 || echo "Directory not found!"'
                 }
             }
@@ -19,6 +20,7 @@ def call(Map config) {
                     echo 'Preparing build context...'
                     sh '''
                     if [ ! -d "jenkins/lab3/static_website" ]; then
+                        echo "Creating static_website directory and copying files..."
                         mkdir -p jenkins/lab3/static_website
                         cp -r /path/to/static_website/* jenkins/lab3/static_website/
                     else
@@ -29,24 +31,30 @@ def call(Map config) {
             }
             stage('Build Docker Image') {
                 steps {
+                    echo 'Building Docker image...'
                     sh "docker build -t ${config.dockerImageName}:${config.dockerImageTag} -f ${config.dockerFilePath} ."
                 }
             }
             stage('Push Docker Image') {
                 steps {
+                    echo 'Pushing Docker image to registry...'
                     withCredentials([usernamePassword(credentialsId: config.dockerHubCredentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-                        sh "echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin"
-                        sh "docker push ${config.dockerImageName}:${config.dockerImageTag}"
+                        sh '''
+                        echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+                        docker push ${config.dockerImageName}:${config.dockerImageTag}
+                        '''
                     }
                 }
             }
             stage('Remove Local Docker Image') {
                 steps {
-                    sh "docker rmi ${config.dockerImageName}:${config.dockerImageTag}"
+                    echo 'Removing local Docker image...'
+                    sh "docker rmi ${config.dockerImageName}:${config.dockerImageTag} || echo 'Image not found locally.'"
                 }
             }
             stage('Deploy to Kubernetes') {
                 steps {
+                    echo 'Deploying to Kubernetes...'
                     withCredentials([string(credentialsId: config.k8sTokenCredentialsId, variable: 'K8S_TOKEN')]) {
                         sh "kubectl --token=$K8S_TOKEN apply -f ${config.kubeDeploymentFile}"
                     }
@@ -55,5 +63,6 @@ def call(Map config) {
         }
     }
 }
+
 
 
